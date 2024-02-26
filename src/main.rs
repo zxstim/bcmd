bb2ca47e7cd6985a0caa37beac954b30a2790464
ZxStim <thang.victor.luong@gmail.com>
2024-02-27 00:22:15 +0700

    updated cli on unused Result

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ fn main() -> Result<()> {
 fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) {
     for line in content.lines() {
         if line.contains(pattern) {
-            writeln!(writer, "{}", line);
+            writeln!(writer, "{}", line).expect("Failed to write to buffer");
         }
     }
 }
